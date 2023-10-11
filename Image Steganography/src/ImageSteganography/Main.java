6505369abf4ad9d2b6a88aad8037126049e10bb6
Girish Mondal <girishmondal.28@gmail.com>
2023-10-11 22:19:36 +0600

    All files added

---
+++ Image Steganography/src/ImageSteganography/Main.java
@@ -0,0 +1,7 @@
+package ImageSteganography;
+public class Main {
+    public static void main(String arg[]) {
+        SplashScreenFrame a=new SplashScreenFrame();
+        a.setVisible(true);
+        a.setLocationRelativeTo(null);
+    }}
